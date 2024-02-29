310e99a10e6a834aea0ba1f549b6d42566e07615
mythili-b <159561083+mythili-b@users.noreply.github.com>
2024-02-29 20:57:14 +0530

    Delete MyComponentWithoutJSX.js

--- MyComponentWithoutJSX.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// Component without JSX
-const MyComponentWithoutJSX = () => {
- return React.createElement('div', null, [
- React.createElement('h2', null, 'Welcome..'),
- React.createElement('p', null, 'This is React component without JSX.'),
- ]);
-}
-export default MyComponentWithoutJSX;
